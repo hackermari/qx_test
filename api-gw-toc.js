782d24d732db63bd6655ed4dd972438005b9f9a3
hackermari <108642783+hackermari@users.noreply.github.com>
2024-07-26 02:47:05 +0800

    Update api-gw-toc.js

--- api-gw-toc.js
+++ api-gw-toc.js
@@ -6,7 +6,7 @@ let obj = JSON.parse(body);
 console.log("1: ", JSON.stringify(obj));
 
 // 修改 data 字段的值
-obj.data = "modified data";  // 你可以根据需要修改为其他值，例如新的时间戳
+obj.data = "2024-07-26 10:00:54";  // 你可以根据需要修改为其他值，例如新的时间戳
 
 // 打印修改后的返回值到日志
 console.log("2: ", JSON.stringify(obj));
