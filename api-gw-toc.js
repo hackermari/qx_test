133c1665586cb8c2b6f6b6530f0054b9509e6a73
hackermari <108642783+hackermari@users.noreply.github.com>
2024-07-26 02:27:37 +0800

    Update api-gw-toc.js

--- api-gw-toc.js
+++ api-gw-toc.js
@@ -6,7 +6,7 @@ let obj = JSON.parse(body);
 obj.data = "modified data";  // 示例：修改返回数据中的 data 字段
 
 // 打印原始和修改后的返回值到日志
-console.log("Original Response: ", $response.body);
+console.log("Original Response: ", obj);
 console.log("Modified Response: ", JSON.stringify(obj));
 
 body = JSON.stringify(obj);
