001054f3666dd07515b9401771b2793167f6aaca
hackermari <108642783+hackermari@users.noreply.github.com>
2024-07-27 01:16:47 +0800

    Update api-gw-toc.js

--- api-gw-toc.js
+++ api-gw-toc.js
@@ -5,11 +5,23 @@ let obj = JSON.parse(body);
 // 打印原始返回值到日志
 console.log("1: ", JSON.stringify(obj));
 
+// 获取当前日期，并设置时间为10:00:00
+let currentDate = new Date();
+currentDate.setHours(10, 0, 0, 0);  // 设置时间为10:00:00
+
+// 格式化日期为 "YYYY-MM-DD HH:mm:ss"
+let formattedDate = currentDate.getFullYear() + '-' + 
+    ('0' + (currentDate.getMonth() + 1)).slice(-2) + '-' + 
+    ('0' + currentDate.getDate()).slice(-2) + ' ' + 
+    ('0' + currentDate.getHours()).slice(-2) + ':' + 
+    ('0' + currentDate.getMinutes()).slice(-2) + ':' + 
+    ('0' + currentDate.getSeconds()).slice(-2);
+
 // 修改 data 字段的值
-obj.data = "2024-07-26 10:00:54";  // 你可以根据需要修改为其他值，例如新的时间戳
+obj.data = formattedDate;
 
 // 打印修改后的返回值到日志
-console.log("2: ", JSON.stringify(obj));
+console.log(formattedDate);
 
 body = JSON.stringify(obj);
 $done({body});
