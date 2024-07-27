7169a27b1ffc4ca59531fbccc26e9331c300abe3
hackermari <108642783+hackermari@users.noreply.github.com>
2024-07-27 13:36:55 +0800

    Update taskMsg.js

--- taskMsg.js
+++ taskMsg.js
@@ -2,17 +2,15 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// 打印原始返回值到日志
-console.log("Original Response: ", JSON.stringify(obj));
-
-// 在这里修改返回值，根据需要修改 obj 内容
-// 例如，修改某个字段的值
-if (obj.data) {
-    obj.data.message = "Modified message content"; // 示例：修改 data.message 字段的值
+// 修改 consumeCycleStore 的值
+if (obj.data && obj.data.taskReachMsgList) {
+    obj.data.taskReachMsgList.forEach(task => {
+        task.consumeCycleStore = "0";  // 示例：将 consumeCycleStore 修改为 1000
+    });
 }
 
 // 打印修改后的返回值到日志
-console.log("Modified Response: ", JSON.stringify(obj));
+console.log(JSON.stringify(obj));
 
 body = JSON.stringify(obj);
 $done({body});
