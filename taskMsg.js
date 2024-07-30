3da3404d467f1908b7618816c587fee21557462b
hackermari <108642783+hackermari@users.noreply.github.com>
2024-07-30 10:43:54 +0800

    Update taskMsg.js

--- taskMsg.js
+++ taskMsg.js
@@ -1,16 +1,28 @@
 // taskMsg.js
 let body = $response.body;
-let obj = JSON.parse(body);
+let obj;
 
-// 修改 consumeCycleStore 的值
-if (obj.data && obj.data.taskReachMsgList) {
-    obj.data.taskReachMsgList.forEach(task => {
-        task.consumeCycleStore = "0";  // 示例：将 consumeCycleStore 修改为 1000
-    });
+try {
+    let obj = JSON.parse(body);
+    
+    // 打印原始返回值到日志
+    console.log(JSON.stringify(obj));
+    
+    // 修改 consumeCycleStore 的值
+    if (obj && obj.data && obj.data.taskReachMsgList) {
+        obj.data.taskReachMsgList.forEach(task => {
+            task.consumeCycleStore = "0";  // 示例：将 consumeCycleStore 修改为 1000
+        });
+        
+        // 打印修改后的返回值到日志
+        console.log(JSON.stringify(obj));
+        
+        body = JSON.stringify(obj);
+    } else {
+        console.log("No taskReachMsgList found in the response.");
+    }
+} catch (e) {
+    console.log(e.message);
 }
-
-// 打印修改后的返回值到日志
-console.log(JSON.stringify(obj));
-
-body = JSON.stringify(obj);
+    
 $done({body});
