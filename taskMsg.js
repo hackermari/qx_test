8160d1ae3f7c3ec864af2377c66f01c9c121a89e
hackermari <108642783+hackermari@users.noreply.github.com>
2024-07-30 10:55:52 +0800

    Update taskMsg.js

--- taskMsg.js
+++ taskMsg.js
@@ -2,27 +2,33 @@
 let body = $response.body;
 let obj;
 
-try {
-    let obj = JSON.parse(body);
+if(body && body !== undefined){
     
-    // 打印原始返回值到日志
-    console.log(JSON.stringify(obj));
-    
-    // 修改 consumeCycleStore 的值
-    if (obj && obj.data && obj.data.taskReachMsgList) {
-        obj.data.taskReachMsgList.forEach(task => {
-            task.consumeCycleStore = "0";  // 示例：将 consumeCycleStore 修改为 1000
-        });
+    try {
+        let obj = JSON.parse(body);
         
-        // 打印修改后的返回值到日志
+        // 打印原始返回值到日志
         console.log(JSON.stringify(obj));
         
-        body = JSON.stringify(obj);
-    } else {
-        console.log("No taskReachMsgList found in the response.");
+        // 修改 consumeCycleStore 的值
+        if (obj && obj.data && obj.data.taskReachMsgList) {
+            obj.data.taskReachMsgList.forEach(task => {
+                task.consumeCycleStore = "0";  // 示例：将 consumeCycleStore 修改为 1000
+            });
+            
+            // 打印修改后的返回值到日志
+            console.log(JSON.stringify(obj));
+            
+            body = JSON.stringify(obj);
+        } else {
+            console.log("No taskReachMsgList found in the response.");
+        }
+    } catch (e) {
+        console.log(e.message);
     }
-} catch (e) {
-    console.log(e.message);
-}
+}else{
+    console.log(body);
     
+}
+
 $done({body});
