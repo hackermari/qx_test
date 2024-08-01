caee69c3e706215955c743fd1a560354fd4a1a41
hackermari <108642783+hackermari@users.noreply.github.com>
2024-08-01 17:49:31 +0800

    Create activityGet.js

---
+++ activityGet.js
@@ -0,0 +1,30 @@
+// activityGet.js
+let body = $response.body;
+let obj;
+
+if(body && body !== undefined){
+    try {
+        let obj = JSON.parse(body);
+        
+        // 打印原始返回值到日志
+        console.log(JSON.stringify(obj));
+        
+        // 修改 consumeCycleStore 的值
+        if (obj && obj.data) {
+            obj.data.onAgingTime = true;
+            
+            // 打印修改后的返回值到日志
+            console.log(JSON.stringify(obj));
+            
+            body = JSON.stringify(obj);
+        } else {
+            console.log("No taskReachMsgList found in the response.");
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+}else{
+    console.log(body);
+}
+
+$done({body});
