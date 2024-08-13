364ecc5e83205c54c5929d544ed06f709ca549bd
hackermari <108642783+hackermari@users.noreply.github.com>
2024-08-13 10:14:03 +0800

    Create getActCycleInfo.js

---
+++ getActCycleInfo.js
@@ -0,0 +1,31 @@
+// getActCycleInfo.js
+let body = $response.body;
+let obj;
+
+if(body && body !== undefined){
+    try {
+        obj = JSON.parse(body);
+    
+        // 打印原始返回值到日志
+        console.log("Original Response: ", JSON.stringify(obj));
+    
+        // 修改 serverTime 的值为 actCycleDetails 最后一个元素的 beginTime
+        if (obj && obj.data && obj.data.actCycleDetails && obj.data.actCycleDetails.length > 0) {
+            let lastCycle = obj.data.actCycleDetails[obj.data.actCycleDetails.length - 1];
+            obj.data.serverTime = lastCycle.beginTime;
+    
+            // 打印修改后的返回值到日志
+            console.log("Modified Response: ", JSON.stringify(obj));
+    
+            body = JSON.stringify(obj);
+        } else {
+            console.log("No data field found in the response.");
+        }
+    } catch (e) {
+        console.log("JSON Parse error: ", e.message);
+    }
+}else{
+    console.log(body);
+}
+
+$done({body});
