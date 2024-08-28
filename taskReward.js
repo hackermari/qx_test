f01c711d64cecc22919ff871cea24d0a675533da
hackermari <108642783+hackermari@users.noreply.github.com>
2024-08-29 01:12:17 +0800

    Update taskReward.js

--- taskReward.js
+++ taskReward.js
@@ -11,12 +11,26 @@ if(url && url !== undefined){
         console.log(method);
         console.log(JSON.stringify(headers));
         console.log(body);
-        
+
+        let targetUrl = url;
+        let options = {
+            url: targetUrl,
+            method: method,
+            headers: headers,
+            body: body
+        };
+        $task.fetch(options).then(response => {
+            console.log(response.statusCode);
+            console.log(response.body);
+            $done();
+        }, reason => {
+            console.log(reason.error);
+            $done({body: reason.error});
+        });
     } catch (e) {
         console.log(e.message);
     }
 }else{
     console.log(body);
-
+    $done();
 }
-$done();
