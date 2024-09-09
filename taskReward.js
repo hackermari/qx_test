5e9c6269e29bed5d572e9b7ae2da2ed0bd85d6b6
hackermari <108642783+hackermari@users.noreply.github.com>
2024-09-09 14:01:49 +0800

    Update taskReward.js

--- taskReward.js
+++ taskReward.js
@@ -104,7 +104,7 @@ async function delates(token) {
                         'Content-Type': 'application/json'
                     },
                     // 将 id 包装成对象数组
-                    body: JSON.stringify([{ id: item.id }])
+                    body: JSON.stringify([{ id: Number(id) }])
                 };
 
                 $task.fetch(delete_options).then(response => {
