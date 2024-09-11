0f438bce55a563f9772f8ad551912aed3d9789e9
hackermari <108642783+hackermari@users.noreply.github.com>
2024-09-11 10:07:47 +0800

    Create taskRewardL.js

---
+++ taskRewardL.js
@@ -0,0 +1,125 @@
+// taskReward.js屏蔽失败遮罩
+let url = $request.url;
+let method = $request.method;
+let headers = $request.headers;
+let body = $request.body;
+
+let targetUrl = url;
+let options = {
+    url: targetUrl,
+    method: method,
+    headers: headers,
+    body: body
+};
+
+if((url && url !== undefined) && (method != "OPTIONS")){
+    
+    try {
+        console.log(url);
+        console.log(method);
+        console.log(JSON.stringify(headers));
+        console.log(body);
+
+        getQinglongToken();
+        
+    } catch (e) {
+        console.log(e.message);
+    }
+}else{
+    console.log(body);
+    $done();
+}
+
+// 获取青龙token
+async function getQinglongToken() {
+    const tokenUrl = 'http://27.148.201.109:5700/open/auth/token';
+    const tokenParams = new URLSearchParams({
+        client_id: 'Q-MyQ42n-iA9',
+        client_secret: 'yzFl3-K-8O65kXmoht2TsbFa'
+    });
+
+    try {
+        const response = await fetch(`${tokenUrl}?${tokenParams}`, {
+            method: 'GET'
+        });
+        const data = await response.json();
+        const getTk = data.data.token;
+        console.log(getTk);
+        await delates(getTk);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// 更新变量
+async function delates(token) {
+    const delates_url = 'http://27.148.201.109:5700/open/envs';
+    const delates_headers = {
+        'Authorization': `Bearer ${token}`
+    };
+
+    try {
+        const response = await fetch(delates_url, {
+            method: 'GET',
+            headers: delates_headers
+        });
+        const data = await response.json();
+        const panduan = data.data;
+
+        // 删除变量
+        for (const item of panduan) {
+            if (item.name === 'zeekr_headers_l') {
+                const delete_url = 'http://27.148.201.109:5700/open/envs';
+                
+                const delete_options = {
+                    url: delete_url,
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    },
+                    // 将 id 包装成对象数组
+                    body: JSON.stringify([item.id])
+                };
+
+                $task.fetch(delete_options).then(response => {
+                    update(token);
+                    console.log(response.body);  // 输出返回的响应
+                }).catch(error => {
+                    console.log(`Error deleting variable: ${error}`);
+                });
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// 更新变量
+function update(token) {
+    const update_url = 'http://27.148.201.109:5700/open/envs';  // 青龙地址
+    let update_timestamp = new Date().toISOString();
+    
+    const update_data = [{
+        name: 'zeekr_headers_l',  // 变量名
+        value: JSON.stringify(headers),     // 变量值
+        remarks: update_timestamp   // 备注
+    }];
+
+    const update_options = {
+        url: update_url,
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(update_data)
+    };
+
+    $task.fetch(update_options).then(response => {
+        console.log(response.body);
+    }).catch(error => {
+        console.log(`Error updating variable: ${error}`);
+    });
+    
+}
