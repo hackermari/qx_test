557a9987ba07f7f6b9249f44bc813653d56b7530
hackermari <108642783+hackermari@users.noreply.github.com>
2024-11-24 13:50:53 +0800

    Update editStore.js

--- editStore.js
+++ editStore.js
@@ -10,9 +10,28 @@ if(body && body !== undefined){
         console.log(JSON.stringify(obj));
         
         // 修改 data 字段的值为新的日期时间
-        if (obj && obj.data !== undefined) {
-            obj.data = {"sourceId":"sourceyxhd","ownOrgan":"FZ11","activityId":"XXT1684127854","activityCode":"ACT_STORE_SIGN","identities":"LAN1684131341","personIdType":"ADMIN_ID","ownPerson":"1742099963829252096","tourActFlag":"0","personId":"1742099963829252096","storeId":"Z321","createTime":"2024-11-24 10:41:57","validity":"1440","businessType":"999999","needRailFlag":"1","activityType":1,"drawActivityId":"1660642112787451905","expertFlag":"0","intentionSeries":"7440","pathway":"QR_CODE"};
-            
+        if (obj && obj !== undefined) {
+            obj.data = {
+                "sourceId": "sourceyxhd",
+                "ownOrgan": "FZ11",
+                "activityId": "XXT1684127854",
+                "activityCode": "ACT_STORE_SIGN",
+                "identities": "LAN1684131341",
+                "personIdType": "ADMIN_ID",
+                "ownPerson": "1742099963829252096",
+                "tourActFlag": "0",
+                "personId": "1742099963829252096",
+                "storeId": "Z321",
+                "createTime": "2024-11-23 13:41:57",
+                "validity": "1440",
+                "businessType": "999999",
+                "needRailFlag": "1",
+                "activityType": 1,
+                "drawActivityId": "1660642112787451905",
+                "expertFlag": "0",
+                "intentionSeries": "7440",
+                "pathway": "QR_CODE"
+            };
             // 打印修改后的返回值到日志
             console.log(JSON.stringify(obj));
             
