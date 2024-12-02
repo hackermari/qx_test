9b0ff89b8d656f67c1283a44b83d60d8baa6fdad
hackermari <108642783+hackermari@users.noreply.github.com>
2024-12-02 20:33:52 +0800

    Update editStore.js

--- editStore.js
+++ editStore.js
@@ -12,25 +12,30 @@ if(body && body !== undefined){
         // 修改 data 字段的值为新的日期时间
         if (obj && obj !== undefined) {
             obj.data = {
-                "sourceId": "sourceyxhd",
-                "ownOrgan": "FZ11",
-                "activityId": "XXT1684127854",
-                "activityCode": "ACT_STORE_SIGN",
-                "identities": "LAN1684131341",
-                "personIdType": "ADMIN_ID",
-                "ownPerson": "1742099963829252096",
-                "tourActFlag": "0",
-                "personId": "1742099963829252096",
-                "storeId": "Z321",
-                "createTime": "2024-11-23 13:41:57",
-                "validity": "1440",
-                "businessType": "999999",
-                "needRailFlag": "1",
-                "activityType": 1,
-                "drawActivityId": "1660642112787451905",
-                "expertFlag": "0",
-                "intentionSeries": "7440",
-                "pathway": "QR_CODE"
+              "code" : "000000",
+              "data" : {
+                "businessType" : "999999",
+                "storeId" : "Z367",
+                "tourActFlag" : "0",
+                "activityCode" : "ACT_STORE_SIGN",
+                "personId" : "1816187918083670016",
+                "activityType" : 1,
+                "ownOrgan" : "2023",
+                "activityId" : "XXT1684127854",
+                "sourceId" : "sourceyxhd",
+                "personIdType" : "ADMIN_ID",
+                "drawActivityId" : "1660642112787451905",
+                "identities" : "LAN1684131341",
+                "expertFlag" : "0",
+                "createTime" : "2024-11-30 16:29:15",
+                "ownPerson" : "1816187918083670016",
+                "intentionSeries" : "8897",
+                "pathway" : "QR_CODE",
+                "validity" : 1440,
+                "needRailFlag" : "1"
+              },
+              "msg" : "成功",
+              "date" : "1732956122199"
             };
             // 打印修改后的返回值到日志
             console.log(JSON.stringify(obj));
